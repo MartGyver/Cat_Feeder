048812cfc49235a9c9d709bb5ccef87ddd0eb025
MartGyver <kardell.martin@gmail.com>
2018-07-08 19:22:09 +0200

    added motor test code for motor and driver. Added schematic formotor and driver.

---
+++ Code/Cad_Feeder_Program/Cad_Feeder_Program.ino
@@ -0,0 +1,23 @@
+//initialize variables
+
+void setup() {
+//set pins
+
+
+//read portion size
+
+}
+
+void loop() {
+//if its time, feed
+
+//else if button pressed
+
+//
+
+}
+
+void feed_cat()
+{
+  
+}
